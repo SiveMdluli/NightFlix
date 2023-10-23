02b08b3c969aa316272ce4e3d4caf6160e6e68ea
Sive Mdluli <mdlulisive202@gmail.com>
2023-10-23 15:28:39 +0200

    Configure api link to https, error likely related

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard.jsx';
 
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=ea2f2c12';
+const API_URL = 'https://www.omdbapi.com/?apikey=ea2f2c12';
 
 const App = () => {
   // Creating a new state to move the code from the console log to them actually being rendered.
